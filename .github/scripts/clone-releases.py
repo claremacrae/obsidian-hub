b3cb64c98d1e9b0c419cd1deea5f7fea324044e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-16 21:45:14 +0100

    Remove repetition

--- .github/scripts/clone-releases.py
+++ .github/scripts/clone-releases.py
@@ -12,23 +12,24 @@ from utils import (
 from utils import PLUGINS_JSON_FILE, THEMES_JSON_FILE, OUTPUT_DIR
 
 
+def clone_repo(plugin):
+    repo = plugin.get("repo")
+    branch = plugin.get("branch", "master")
+    user = repo.split("/")[0]
+    print(f"git clone https://github.com/{repo}.git")
+
+
 def process_released_plugins(overwrite=False):
     plugin_list = get_json_from_github(PLUGINS_JSON_FILE)
     for plugin in plugin_list:
-        repo = plugin.get("repo")
-        branch = plugin.get("branch", "master")
-        user = repo.split("/")[0]
-        print(f"git clone https://github.com/{repo}.git")
+        clone_repo(plugin)
 
 
 def process_released_themes(overwrite=False):
     print("-----\nProcessing themes....\n")
     theme_list = get_json_from_github(THEMES_JSON_FILE)
     for theme in theme_list:
-        repo = theme.get("repo")
-        branch = theme.get("branch", "master")
-        user = repo.split("/")[0]
-        print(f"git clone https://github.com/{repo}.git")
+        clone_repo(theme)
 
 
 def main(argv=sys.argv[1:]):
